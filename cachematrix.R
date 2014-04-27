935d94ad94727667e9479d365f508b1161c60b55
lfmoyaa <lfmoyaa@unal.edu.co>
2014-04-26 23:51:51 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,24 @@
-makeCacheMatrix <- function(x = matrix()) { ##creation of special matrix object
-	inv <- NULL   ##inicialization of inverse matrix
-	set <- function(y) {  ##function of set matrix's values.##if matrix changed, then its inverse is initialized again 
-		x <<- y
-		inv <<- NULL   
-	}
-	get <- function() x	 ##return matrix
-	setinv <- function(inverse) inv <<- inverse  ##set matrix inverse
-	getinv <- function() inv ##return inverse of matrix, if matrix inverse hasn't calculated yet then return null
-	list(set = set,get = get, ##list of functions
-		setinv = setinv,
-		getinv = getinv)
+makeCacheMatrix <- function(at = matrix()) { ##creation of matrix
+	inver <- NULL   
+	set <- function(y) {  
+		at <<- y
+		inver <<- NULL   }
+	get <- function() at	 ##return matrix
+	setinver <- function(inverse) inver <<- inverse  ##set matrix inverse
+	getinver <- function() inver ##return inverse of matrix, if matrix inverse hasn't calculated yet then return NULL
+	list(set = set,get = get, setinver = setinver,getinver = getinver)     ##list of functions
+		
+		
 }
 
-cacheSolve <- function(x) { ##function that calcule and return the inverse of special matrix 'x'
-	inv <- x$getinv() ## first get the inverse of matrix
-        if(!is.null(inv)) { ## if inverse has already calculated then return it
+cacheSolve <- function(at) { ##function that calcule and return the inverse of matrix 'at'
+	inver <- at$getinver() ## get the inverse
+        if(!is.null(inver)) { 
                 message("getting cached data")
-                return(inv)
+                return(inver)
         }
-        data <- x$get() ##if not, then get data of x 
-        inv <- solve(data) ##calcule inverse of adquired data
-        x$setinv(inv) ##set the inverse of object x
-        inv	##return the inverse
+        data <- at$get() ##if inverse has not been calculated then get data of ´at´ 
+        inver <- solve(data) ##calcule inverse of data
+        at$setinver(inver) ##set the inverse of object at
+        inver	##return the inverse
 }
